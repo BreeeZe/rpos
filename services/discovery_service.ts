268c95480ccf2f46ba6fc71de356a54a847ea065
Jeroen Versteege <github@breeeze.com>
2016-05-10 11:05:30 +0200

    used typescripts string templating : https://github.com/Microsoft/TypeScript-Handbook/blob/master/pages/Basic%20Types.md#string

--- services/discovery_service.ts
+++ services/discovery_service.ts
@@ -70,38 +70,35 @@ class DiscoveryService {
 
         if (probe_type === "" || probe_type.indexOf("NetworkVideoTransmitter") > -1) {
 
-          let reply = 
-	'<?xml version="1.0" encoding="UTF-8"?>' +
-        '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dn="http://www.onvif.org/ver10/network/wsdl">'+
-	'<SOAP-ENV:Header>'+
-	  '<wsa:MessageID>REPLACE_MESSAGE_ID</wsa:MessageID>'+
-	  '<wsa:RelatesTo>REPLACE_RELATES_TO</wsa:RelatesTo>'+
-	  '<wsa:To SOAP-ENV:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>'+
-	  '<wsa:Action SOAP-ENV:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2005/04/discovery/ProbeMatches</wsa:Action>'+
-          '<d:AppSequence SOAP-ENV:mustUnderstand="true" MessageNumber="68" InstanceId="1460972484"/>'+
-	'</SOAP-ENV:Header>'+
-	'<SOAP-ENV:Body>'+
-	  '<d:ProbeMatches>'+
-            '<d:ProbeMatch>'+
-	      '<wsa:EndpointReference>'+
-		'<wsa:Address>REPLACE_ENDPOINT_UUID</wsa:Address>'+
-		'</wsa:EndpointReference>'+
-		'<d:Types>dn:NetworkVideoTransmitter</d:Types>'+
-		'<d:Scopes>REPLACE_SCOPES</d:Scopes>'+
-		'<d:XAddrs>REPLACE_XADDR_SERVICE</d:XAddrs>'+
-		'<d:MetadataVersion>1</d:MetadataVersion>'+
-	'</d:ProbeMatch>'+
-	'</d:ProbeMatches>'+
-	'</SOAP-ENV:Body>'+
-        '</SOAP-ENV:Envelope>'
-	;
-
-        // Populate the XML reply 
-        reply = reply.replace("REPLACE_RELATES_TO",probe_uuid);
-        reply = reply.replace("REPLACE_MESSAGE_ID",'uuid:' + uuid.v1()); // time based uuid
-        reply = reply.replace("REPLACE_ENDPOINT_UUID",'urn:uuid:' + uuid5(this.config.IpAddress + this.config.ServicePort + this.config.RTSPPort));
-        reply = reply.replace("REPLACE_XADDR_SERVICE","http://"+this.config.IpAddress + ":" + this.config.ServicePort + "/onvif/device_service");
-        reply = reply.replace("REPLACE_SCOPES","onvif://www.onvif.org/type/video_encoder onvif://www.onvif.org/type/ptz   onvif://www.onvif.org/hardware/RaspberryPI onvif://www.onvif.org/name/PI onvif://www.onvif.org/location/" );
+          let reply = `<?xml version="1.0" encoding="UTF-8"?>
+          <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://www.w3.org/2003/05/soap-envelope" xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:dn="http://www.onvif.org/ver10/network/wsdl">
+            <SOAP-ENV:Header>
+              <wsa:MessageID>uuid:${uuid.v1()}</wsa:MessageID>
+              <wsa:RelatesTo>${probe_uuid}</wsa:RelatesTo>
+              <wsa:To SOAP-ENV:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</wsa:To>
+              <wsa:Action SOAP-ENV:mustUnderstand="true">http://schemas.xmlsoap.org/ws/2005/04/discovery/ProbeMatches</wsa:Action>
+              <d:AppSequence SOAP-ENV:mustUnderstand="true" MessageNumber="68" InstanceId="1460972484"/>
+            </SOAP-ENV:Header>
+            <SOAP-ENV:Body>
+              <d:ProbeMatches>
+                <d:ProbeMatch>
+                  <wsa:EndpointReference>
+                    <wsa:Address>urn:uuid:${utils.uuid5(utils.getIpAddress() + this.config.ServicePort + this.config.RTSPPort)}</wsa:Address>
+                  </wsa:EndpointReference>
+                  <d:Types>dn:NetworkVideoTransmitter</d:Types>
+                  <d:Scopes>
+                    onvif://www.onvif.org/type/video_encoder 
+                    onvif://www.onvif.org/type/ptz
+                    onvif://www.onvif.org/hardware/RaspberryPI
+                    onvif://www.onvif.org/name/PI
+                    onvif://www.onvif.org/location/
+                  </d:Scopes>
+                  <d:XAddrs>http://${utils.getIpAddress()}:${this.config.ServicePort}/onvif/device_service</d:XAddrs>
+                  <d:MetadataVersion>1</d:MetadataVersion>
+              </d:ProbeMatch>
+              </d:ProbeMatches>
+            </SOAP-ENV:Body>
+          </SOAP-ENV:Envelope>`;
 
           let reply_bytes = new Buffer(reply);
           return discover_socket.send(reply_bytes, 0, reply_bytes.length, rinfo.port, rinfo.address);
