4eccc4367869e4f581c658e4b7cc8b6bb73ba26d
Jeroen Versteege <github@breeeze.com>
2016-05-10 21:28:11 +0200

    commented gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,12 @@ var gulp = require('gulp'),
 var version = 'rpos-' + pkg.version;
 var releaseDir = 'release/' + version;
 
+//Default task: runs the typings and compile task, started when running "gulp" without any parameters.
 gulp.task('default',function(cb){
     runSequence('typings','compile',cb);
 });
 
+//Compile task: compiles all .ts files to .js and generates sourcemaps to aid in debugging.
 gulp.task('compile', function () {
     return gulp.src(["**/*.ts","!./node_modules/**/*","!./typings/**/*"])
         .pipe(sourcemaps.init())
@@ -23,12 +25,22 @@ gulp.task('compile', function () {
         .pipe(gulp.dest("./"));
 });
 
+//Typings task: Downloads all type definitions used for development.
 gulp.task('typings', function (done) {
     var cwd = process.cwd();
     typings.install({ cwd: cwd, production: false });
     done();
 });
 
+//Release task: generates a release package.
+gulp.task('release', ['copy-release-js', 'copy-release-bin', 'copy-release-modules', 'copy-release-views', 
+	'copy-release-web', 'copy-release-wsdl', 'copy-release-config'], function () {
+	return gulp.src([releaseDir + '/**/*', releaseDir + '/*.zip'])
+		.pipe(zip(version + '.zip'))
+		.pipe(gulp.dest('release'));
+});
+
+// --- all partial taks to generate a release.
 gulp.task('copy-release-js', function () {
 	return gulp.src(['**/*.js', '!release/**', '!gulpfile.js', 'README.md', 'package.json', '!node_modules/gulp*/**'])
 		.pipe(gulp.dest(releaseDir));
@@ -57,10 +69,4 @@ gulp.task('copy-release-web', function () {
 gulp.task('copy-release-wsdl', function () {
 	return gulp.src('wsdl/**/*')
 		.pipe(gulp.dest(releaseDir + '/wsdl'));
-});
-gulp.task('release', ['copy-release-js', 'copy-release-bin', 'copy-release-modules', 'copy-release-views', 
-	'copy-release-web', 'copy-release-wsdl', 'copy-release-config'], function () {
-	return gulp.src([releaseDir + '/**/*', releaseDir + '/*.zip'])
-		.pipe(zip(version + '.zip'))
-		.pipe(gulp.dest('release'));
 });
