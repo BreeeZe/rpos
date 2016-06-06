a3b1959cc128b40c6d15e680a098c86b6658d466
sunnyworm <sunnyworm@gmail.com>
2016-06-06 17:23:17 +0800

    add -y flag to script

--- setup_v4l2rtspserver.sh
+++ setup_v4l2rtspserver.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/mpromonet/v4l2rtspserver
-sudo apt install liblivemedia-dev liblog4cpp5-dev cmake
+sudo apt install -y liblivemedia-dev liblog4cpp5-dev cmake
 cd v4l2rtspserver
 cmake . && make
 sudo make install
