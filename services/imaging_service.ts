5ac41bb1b43a54829e069e2cc8abbdc5e833c59e
Roger Hardiman <opensource@rjh.org.uk>
2018-05-31 19:55:38 +0100

    Add more Profile T Imaging Support for Focus and Brightness
    This fires an event in the main PTZ driver which can then be used
    to control the camera optics

--- services/imaging_service.ts
+++ services/imaging_service.ts
@@ -14,7 +14,11 @@ class ImagingService extends SoapService {
   imaging_service: any;
   callback: any;
 
-  presetArray = [];
+  brightness = 0;
+  autoFocusMode = '';
+  focusNearLimit = 0;
+  focusFarLimit = 0;
+  focusDefaultSpeed = 0;
 
   constructor(config: rposConfig, server: Server, callback) {
     super(config, server);
@@ -29,6 +33,12 @@ class ImagingService extends SoapService {
       wsdlPath: 'wsdl/imaging_service.wsdl',
       onReady: () => console.log('imaging_service started')
     };
+
+    this.brightness = 50;  // range is 0..100
+    this.autoFocusMode = "MANUAL"; // MANUAL or AUTO
+    this.focusDefaultSpeed = 0.5; // range 0.0 to 1.0
+    this.focusNearLimit = 1.0;  // range 0.1 to 3.0
+    this.focusFarLimit = 0.0; // range 0.0 to 0.0
 
     this.extendService();
   }
@@ -106,7 +116,7 @@ class ImagingService extends SoapService {
             Focus : { 
               AutoFocusModes : ['AUTO','MANUAL'],
               DefaultSpeed : { 
-                Min : 1.0,
+                Min : 0.0,
                 Max : 1.0
               },
               NearLimit : { 
@@ -188,18 +198,137 @@ class ImagingService extends SoapService {
     port.GetImagingSettings = (args /*, cb, headers*/) => {
       var GetImagingSettingsResponse = {
         ImagingSettings : {
-          Brightness : 50,
+          Brightness : this.brightness,
           Focus : { 
-            AutoFocusMode : 'AUTO',
-            DefaultSpeed : 1.0,
-            NearLimit : 1.0,
-            FarLimit : 0.0, // Inifinit
+            AutoFocusMode : this.autoFocusMode,
+            DefaultSpeed : this.focusDefaultSpeed,
+            NearLimit : this.focusNearLimit,
+            FarLimit : this.focusFarLimit, // Infinity
             //Extension : { }
             },
         }
       };
       return GetImagingSettingsResponse;
     };
+
+        //var SetImagingSettings = { 
+        //VideoSourceToken : { xs:string},
+        //ImagingSettings : { 
+          //BacklightCompensation : { 
+            //Mode : { xs:string},
+            //Level : { xs:float}
+          //},
+          //Brightness : { xs:float},
+          //ColorSaturation : { xs:float},
+          //Contrast : { xs:float},
+          //Exposure : { 
+            //Mode : { xs:string},
+            //Priority : { xs:string},
+            //Window : { 
+              //attributes : {
+                //bottom : {xs:float},
+                //top : {xs:float},
+                //right : {xs:float},
+                //left : {xs:float}
+              //}
+            //},
+            //MinExposureTime : { xs:float},
+            //MaxExposureTime : { xs:float},
+            //MinGain : { xs:float},
+            //MaxGain : { xs:float},
+            //MinIris : { xs:float},
+            //MaxIris : { xs:float},
+            //ExposureTime : { xs:float},
+            //Gain : { xs:float},
+            //Iris : { xs:float}
+          //},
+          //Focus : { 
+            //AutoFocusMode : { xs:string},
+            //DefaultSpeed : { xs:float},
+            //NearLimit : { xs:float},
+            //FarLimit : { xs:float},
+            //Extension : { }
+          //},
+          //IrCutFilter : { xs:string},
+          //Sharpness : { xs:float},
+          //WideDynamicRange : { 
+            //Mode : { xs:string},
+            //Level : { xs:float}
+          //},
+          //WhiteBalance : { 
+            //Mode : { xs:string},
+            //CrGain : { xs:float},
+            //CbGain : { xs:float},
+            //Extension : { }
+          //},
+          //Extension : { 
+            //ImageStabilization : { 
+              //Mode : { xs:string},
+              //Level : { xs:float},
+              //Extension : { }
+            //},
+            //Extension : { 
+              //IrCutFilterAutoAdjustment : [{ 
+                //BoundaryType : { xs:string},
+                //BoundaryOffset : { xs:float},
+                //ResponseTime : { xs:duration},
+                //Extension : { }
+              //}],
+              //Extension : { 
+                //ToneCompensation : { 
+                  //Mode : { xs:string},
+                  //Level : { xs:float},
+                  //Extension : { }
+                //},
+                //Defogging : { 
+                  //Mode : { xs:string},
+                  //Level : { xs:float},
+                  //Extension : { }
+                //},
+                //NoiseReduction : { 
+                  //Level : { xs:float}
+                //},
+                //Extension : { }
+              //}
+            //}
+          //}
+        //},
+        //ForcePersistence : [{ xs:boolean}]
+      //
+      //};
+
+      port.SetImagingSettings = (args) => {
+        var SetImagingSettingsResponse = { };
+
+        // Check for Brightness value
+        if (args.ImagingSettings) {
+          if (args.ImagingSettings.Brightness) {
+            this.brightness = args.ImagingSettings.Brightness;
+            // emit the 'brightness' message to the parent
+            if (this.callback) this.callback('brightness', this.brightness);
+          }
+          if (args.ImagingSettings.Focus) {
+            if (args.ImagingSettings.Focus.AutoFocusMode) {
+              this.autoFocusMode = args.ImagingSettings.Focus.AutoFocusMode;
+              if (this.callback) this.callback('focusmode', this.autoFocusMode);
+            }
+            if (args.ImagingSettings.Focus.DefaultSpeed) {
+              this.focusDefaultSpeed = args.ImagingSettings.Focus.DefaultSpeed;
+              if (this.callback) this.callback('focusdefaultspeed', this.focusDefaultSpeed);
+            }
+            if (args.ImagingSettings.Focus.NearLimit) {
+              this.focusNearLimit = args.ImagingSettings.Focus.NearLimit;
+              if (this.callback) this.callback('focusnearlimit', this.focusNearLimit);
+            }
+            if (args.ImagingSettings.Focus.FarLimit) {
+              this.focusFarLimit = args.ImagingSettings.Focus.FarLimit;
+              if (this.callback) this.callback('focusfarlimit', this.focusFarLimit);
+            }
+          }
+        }
+
+        return SetImagingSettingsResponse;
+      };
 
   }
 }
