206c53aa1197edfafc6f0f7fc9b5fd8d755cab8a
Roger Hardiman <opensource@rjh.org.uk>
2018-12-30 02:22:08 +0000

    Improve Authentication checking and error reply
    Error Reply - validates OK with SoapUI
    Could not get the 'ter' namespace in the Envelope so have added it
    to the Body
    
    Can ignore Authentication on GetSystemDateAndTime as per ONVIF Spec
    
    Also handles case were Nonce and Password may be the actual string,
    or the string may be in a $value field (if the XML item had
    attributes associated with it)

--- lib/SoapService.ts
+++ lib/SoapService.ts
@@ -7,6 +7,29 @@ import { Server } from 'http';
 var soap = <any>require('soap');
 var utils = Utils.utils;
 
+var NOT_IMPLEMENTED = {
+  Fault: {
+    attributes: { // Add namespace here. Really wanted to put it in Envelope but this should be valid
+      'xmlns:ter' : 'http://www.onvif.org/ver10/error',
+    },
+    Code: {
+      Value: "soap:Sender",
+      Subcode: {
+        Value: "ter:NotAuthorized",  
+      },
+    },
+    Reason: {
+      Text: {
+        attributes: {
+          'xml:lang': 'en',
+        },
+        $value: 'Sender not Authorized',
+      }
+    }
+  }
+};
+
+
 class SoapService {
   webserver: Server;
   config: rposConfig;
@@ -47,12 +70,20 @@ class SoapService {
       onReady();
     };
     this.serviceInstance = soap.listen(this.webserver, this.serviceOptions);
-    if (this.config.Username) {
-      this.serviceInstance.authenticate = (security) => {
-        var token = security.UsernameToken;
+
+    this.serviceInstance.on('headers', (headers, methodName) => {
+      // Use the '=>' notation so 'this' refers to the class we are in
+      // ONVIF allows GetSystemDateAndTime to be sent with no authenticaton header
+      // So we check the header and check authentication in this function
+
+      // utils.log.info('received soap header');
+      if (methodName === "GetSystemDateAndTime") return;
+
+      if (this.config.Username) {
+        var token = headers.Security.UsernameToken;
         var user = token.Username;
-        var password = token.Password.$value;
-        var nonce = token.Nonce.$value;
+        var password = (token.Password.$value || token.Password);
+        var nonce = (token.Nonce.$value || token.Nonce); // handle 2 ways to map XML to the javascript data structure
         var created = token.Created;
 
         var onvif_username = this.config.Username;
@@ -67,10 +98,15 @@ class SoapService {
         pwHash.update(combined_data);
         var generated_password = pwHash.digest('base64');
 
-        return (user === onvif_username && password === generated_password);
-      };
-    }
+        var password_ok = (user === onvif_username && password === generated_password);
 
+        if (password_ok == false) {
+          utils.log.info('Invalid username/password with ' + methodName);
+          throw NOT_IMPLEMENTED;
+        }
+      };
+    });
+    
     this.serviceInstance.on("request", (request: any, methodName: string) => {
       utils.log.debug('%s received request %s', (<TypeConstructor>this.constructor).name, methodName);
     });
