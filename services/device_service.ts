44d5badaa6ea8eb16af177c8cc5a6d332ebd0c62
Roger Hardiman <software@rjh.org.uk>
2023-06-19 14:04:43 +0100

    Add network messages required by ODM

--- services/device_service.ts
+++ services/device_service.ts
@@ -260,6 +260,17 @@ class DeviceService extends SoapService {
       return SetHostnameFromDHCPResponse;
     };
 
+    port.GetDNS = (args /*, cb, headers*/) => {
+      var GetDNSResponse = { 
+        DNSInformation : { 
+          FromDHCP : true,
+          Extension : { }
+        }
+      
+      };
+      return GetDNSResponse;
+    };
+
     port.GetScopes = (args) => {
       var GetScopesResponse = { Scopes: [] };
       GetScopesResponse.Scopes.push({
@@ -280,6 +291,14 @@ class DeviceService extends SoapService {
       return GetScopesResponse;
     };
 
+
+    port.GetDiscoveryMode = (args /*, cb, headers*/) => {
+      var GetDiscoveryModeResponse = { 
+        DiscoveryMode : true
+      };
+      return GetDiscoveryModeResponse;
+    };
+    
     port.GetServiceCapabilities = (args /*, cb, headers*/) => {
       var GetServiceCapabilitiesResponse = {
         Capabilities: {
@@ -344,9 +363,28 @@ class DeviceService extends SoapService {
     };
 
     port.GetNTP = (args /*, cb, headers*/) => {
-      var GetNTPResponse = {};
-      return GetNTPResponse;
-    };
+       var GetNTPResponse = { 
+          NTPInformation : { 
+            FromDHCP : false,
+            //NTPFromDHCP : [{ 
+            //  Type : { xs:string},
+            //  IPv4Address : { xs:token},
+            //  IPv6Address : { xs:token},
+            //  DNSname : { xs:token},
+            //  Extension : { }
+            //}],
+            NTPManual : [{ 
+              Type : "DNS",
+              //IPv4Address : { xs:token},
+              //IPv6Address : { xs:token},
+              DNSname : "pool.ntp.org",
+              Extension : { }
+            }],
+            Extension : { }
+           } 
+        };
+        return GetNTPResponse;
+      };
 
     port.SetNTP = (args /*, cb, headers*/) => {
       var SetNTPResponse = {};
@@ -401,6 +439,25 @@ class DeviceService extends SoapService {
         }]
       };
       return GetNetworkProtocolsResponse;
+    };
+
+    port.GetNetworkDefaultGateway = (args /*, cb, headers*/) => {
+      let GetNetworkDefaultGatewayResponse = {}
+        if (utils.isLinux) {
+        // Linux method for now. Need to include Windows and Mac
+        const spawn = require('child_process').spawnSync;
+
+        const child = spawn('bash', ['-c', 'ip route']).stdout.toString();
+        const gateway = child.match(/default via (.*?)\s/)[1]; // Look for text "default via " and then get everything up to the next Space or Tab
+        GetNetworkDefaultGatewayResponse = { 
+          NetworkGateway : { 
+            IPv4Address : [gateway], // FIXME. Need to ask the OS for this information
+          //IPv6Address : [{ xs:token}]
+          }
+        };
+      } else {
+        return GetNetworkDefaultGatewayResponse;
+      }
     };
 
     port.GetRelayOutputs = (args /*, cb, headers*/) => {
